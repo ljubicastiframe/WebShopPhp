3ebe14a47b4edd455b4c96329d32ade45c5ffb86
Ljubicasti <60853843+ljubicastiframe@users.noreply.github.com>
2022-05-24 12:19:47 +0200

    Add files via upload

---
+++ webShop/config.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "webShop_projekat";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+?>
